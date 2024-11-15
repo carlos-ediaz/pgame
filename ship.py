cbe4e6b7e260a71cf1cba2efd11ce9eb479da8e5
Carlos Diaz <carlos97dt@gmail.com>
2024-11-15 14:37:02 -0500

    Spaceship moving with edges

--- ship.py
+++ ship.py
@@ -4,6 +4,7 @@ class Ship:
     def __init__(self, ai_game):
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         #load the ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
@@ -15,22 +16,29 @@ class Ship:
         #Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #Decimal value for the ship's position
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
         #Movement flag
         self.moving_right = False
         self.moving_left = False
         self.moving_up = False
         self.moving_down = False
-        
+
     def update(self):
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
-        if self.moving_up:
-            self.rect.y -= 1
-        if self.moving_down:
-            self.rect.y += 1
-    
+        print(self.screen_rect)
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+        self.rect.x = self.x
+        self.rect.y = self.y
+
     def blitme(self):
         #Draw the ship at its current location
         self.screen.blit(self.image, self.rect)
