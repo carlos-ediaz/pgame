62531617a118028c748a90cf7b71ae77943b7486
Carlos Diaz <carlos97dt@gmail.com>
2024-11-19 21:07:46 -0500

    Server file

---
+++ server.py
@@ -0,0 +1,14 @@
+import os
+from http.server import SimpleHTTPRequestHandler
+from socketserver import TCPServer
+
+PORT = int(os.environ.get("PORT", 8000))  # Puerto dinámico de Heroku
+DIRECTORY = "build"  # Carpeta generada por Pygbag
+
+class Handler(SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+with TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at port {PORT}")
+    httpd.serve_forever()
